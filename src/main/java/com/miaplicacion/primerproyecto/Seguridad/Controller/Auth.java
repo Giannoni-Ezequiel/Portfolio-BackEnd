b9a9cc186441791d4bca6a680cc9350939469410
Ezequiel Giannoni <egiannoni@frba.utn.edu.ar>
2022-08-21 23:20:56 -0300

    BackEnd

--- src/main/java/com/miaplicacion/primerproyecto/Seguridad/Controller/Auth.java
+++ src/main/java/com/miaplicacion/primerproyecto/Seguridad/Controller/Auth.java
@@ -25,7 +25,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://bkdprueba95.herokuapp.com/")
 @RequestMapping("/auth")
 public class Auth {
 
