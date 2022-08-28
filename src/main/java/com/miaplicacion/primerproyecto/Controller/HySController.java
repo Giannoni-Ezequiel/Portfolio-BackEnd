f6b0242aab127d960a90764dfcb03c874854867d
Ezequiel <egiannoni@frba.utn.edu.ar>
2022-08-28 18:59:50 -0300

    ConfigCors Heroku

--- src/main/java/com/miaplicacion/primerproyecto/Controller/HySController.java
+++ src/main/java/com/miaplicacion/primerproyecto/Controller/HySController.java
@@ -1,10 +1,10 @@
 package com.miaplicacion.primerproyecto.Controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://proyectoangulareg.web.app/")
+
 public class HySController 
 {
     
--- src/main/java/com/miaplicacion/primerproyecto/Controller/PersonaController.java
+++ src/main/java/com/miaplicacion/primerproyecto/Controller/PersonaController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://proyectoangulareg.web.app/")
+
 public class PersonaController {
     
     /*List<Persona> listaPersonas = new ArrayList();
