ffaea676139bd2d7de50fc448ba641430d44d675
Ezequiel <egiannoni@frba.utn.edu.ar>
2022-08-29 14:45:18 -0300

    Corscorscors02

--- src/main/java/com/miaplicacion/primerproyecto/EzequielGApplication.java
+++ src/main/java/com/miaplicacion/primerproyecto/EzequielGApplication.java
@@ -20,7 +20,7 @@ public class EzequielGApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("https://proyectoangulareg.web.app"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
